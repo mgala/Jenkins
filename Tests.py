b8ee724ffa3373d0b339a90e0526e045dcf84493
Mateusz <mgala@student.tgm.ac.at>
2016-02-09 11:10:47 +0100

    repaired errors

--- Tests.py
+++ Tests.py
@@ -1,40 +1,40 @@
-from csv import *
+from csv1 import *
 from unittest import TestCase
 
 __author__ = 'Mateusz Gala'
 class Tests(TestCase):
     def setUp(self):
-        self.csv = Tests()
-        self.f1 = self.csv.open_file("input1.csv", "rt")
-        self.f2 = self.csv.open_file("input2.csv", "rt")
+        self.csv = csv1()
+        self.f1 = self.csv.openfile("Book1.csv", "rt")
+        self.f2 = self.csv.openfile("Book2.csv", "rt")
 
-        self.d1 = self.csv.get_dialect(self.f1)
-        self.d2 = self.csv.get_dialect(self.f2)
+        self.d1 = self.csv.dialekt(self.f1)
+        self.d2 = self.csv.dialekt(self.f2)
 
-        self.r1 = self.csv.read_file(self.f1, self.d1)
-        self.r2 = self.csv.read_file(self.f2, self.d2)
+        self.r1 = self.csv.read(self.f1, self.d1)
+        self.r2 = self.csv.read(self.f2, self.d2)
 
-        self.out = self.csv.append_files(self.r1, self.r2)
+        self.out = self.csv.mergefiles(self.r1, self.r2)
 
         self.read_test = ['1', '2', '3', 'zweite zeile']
         self.out_test = [['1', '2', '3', 'zweite zeile'], ['1', '2', '3',]]
         pass
 
     def test_open_file(self):
-        self.assertRaises(FileNotFoundError, self.csv.open_file, "irgendwas.csv", "rt")
+        self.assertRaises(FileNotFoundError, self.csv.openfile, "irgendwas.csv", "rt")
 
     def test_read_file(self):
-        reader = self.csv.read_file(self.f1, self.d1)
+        reader = self.csv.read(self.f1, self.d1)
         for row in reader:
             assert(row == self.read_test)
 
     def test_append_files(self):
-        out = self.csv.append_files(self.f1, self.f2)
+        out = self.csv.mergefiles(self.f1, self.f2)
         for row in out:
             assert(row == self.out_test)
 
     def test_write_file(self):
-        self.assertRaises(TypeError, self.csv.write_file, "wrongType", "semicolon", self.out)
+        self.assertRaises(TypeError, self.csv.outputfile, "wrongType", "semicolon", self.out)
 
     def test_close_file(self):
         self.assertRaises(AttributeError, self.csv.close_file, "wrongAttribute")
--- csv1.py
+++ csv1.py
@@ -6,38 +6,47 @@ und das ausgeben davon.
 __author__ = "Mateusz Gala"
 __version__ = "1.0"
 
-class jenkins:
+class csv1:
     # Oeffnen der beiden Dateien.
+    @staticmethod
     def openfile(name, like):
         return open(name, like)
 
     # Der Dialekt wird mittels eines Sniffers geholt
+    @staticmethod
     def dialekt(f):
         dialect = csv.Sniffer().sniff(f.read(1024))
         f.seek(0)
+        return dialect
 
-
+    @staticmethod
     def set_delimiter(name,delimiter):
-        csv.register_dialect(name, delimiter)
+        return csv.register_dialect(name, delimiter)
 
     # Das File wird ausgelesen mit dem erfassten Dialekt.
+    @staticmethod
     def read(f, dialect):
         r = csv.reader(f, dialect)
+        return r
 
     #die files werden in 1 file umgewandelt
+    @staticmethod
     def mergefiles(r1,r2):
         output = []
         for row in r1:
             output.append(row)
         for row in r2:
             output.append(row)
+        return output
 
     #ein outputfile wird erstellt
     #in das neue file wird mit einem beliebigen dialekt geschrieben
+    @staticmethod
     def outputfile(outputfilename, dialekt, output):
         writer = csv.writer(outputfilename, dialekt)
         for all in output:
             writer.writerow(all)
-
+        return writer
+    @staticmethod
     def close_file(file):
         file.close()
